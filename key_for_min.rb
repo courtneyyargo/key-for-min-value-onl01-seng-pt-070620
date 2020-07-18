0bdce6bced87e0afedb6e88e46a7dcc1bf4f135a
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-18 17:39:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,12 @@ def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil
   name_hash.each do |k, v|
-  if 
+  if min_value == nil || v = min_value
+    min_key = k 
+    min_value = v 
+  end
+end
+min_key
 end
 
 
