74cc30f2168790c68abe58764a63723683a4a0cb
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-18 17:37:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,10 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  name_hash.each do |min_key, min_value|
-  name_hash.invert
+  min_key = nil 
+  min_value = nil
+  name_hash.each do |k, v|
+  if 
 end
 
 
